6f94a2637c28e52b34ec16eeda9db50160028909
Bella <98971465+Bellawareness@users.noreply.github.com>
2022-04-06 15:17:25 +0000

    added certificates

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const Iam = {
-  Name: "A collaboration with 5 team members from Generation, to create a restaurant's menu using HTML & CSS. ",
-  Skills : "JavaScript code that returns a string with no vowels. Utilized procreate to create background and CSS to style HTML. ",
-    project :" HTML, CSS and bootstrap to create a responsive website.",
-  
-  //removed the link because I wanted it clickable
-  //link : //"https://bellawareness.github.io/bellasPortfolio/",
+  Name: "A collaboration with 5 team members from Generation, to create a restaurant's menu used HTML & CSS.",
+  Skills : "A code that returns a string with no vowels. Utilized procreate to create background and CSS to style HTML. ",
+    project :" used HTML, CSS and bootstrap to create a responsive website.",
+   api :" Learning to use Api and minipulating the CSS and HTML.",
+
   
   //a function to call list and input info needed for homework 
   
   myPortfoilo : function() {
-    return "•" + "The Team Work Project: " + this.Name  +  "•" + " JavaScript Project: " + this.Skills + "•" + "HTML Project:" + this.project ; 
+    return "•" + "Team Work Project: " + this.Name +   "•" + "JavaScript Project: " + this.Skills + "•" + "HTML Project:" + this.project +  "•" + "Nasa Project:"+ this.api; 
   }
 };
 
