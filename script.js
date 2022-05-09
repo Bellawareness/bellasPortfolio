89948cc936220e683082bf209f61f8258ab1e2a4
Bella <98971465+Bellawareness@users.noreply.github.com>
2022-05-09 19:27:15 +0000

    added passion project

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-const Iam = {
-  Name: "A collaboration with 5 team members from Generation, to create a restaurant's menu used HTML & CSS.",
-  Skills : "A code that returns a string with no vowels. Utilized procreate to create background and CSS to style HTML. ",
-    project :" used HTML, CSS and bootstrap to create a responsive website.",
-   api :" Learning to use Api and minipulating the CSS and HTML.",
+// const Iam = {
+//   Name: "A collaboration with 5 team members from Generation, to create a restaurant's menu used HTML & CSS.",
+//   Skills : "A code that returns a string with no vowels. Utilized procreate to create background and CSS to style HTML. ",
+//     project :" used HTML, CSS and bootstrap to create a responsive website.",
+//    api :" Learning to use Api and minipulating the CSS and HTML.",
 
   
-  //a function to call list and input info needed for homework 
+//   //a function to call list and input info needed for homework 
   
-  myPortfoilo : function() {
-    return "•" + "Team Work Project: " + this.Name +   "•" + "JavaScript Project: " + this.Skills + "•" + "HTML Project:" + this.project +  "•" + "Nasa Project:"+ this.api; 
-  }
-};
+//   myPortfoilo : function() {
+//     return "•" + "Team Work Project: " + this.Name +   "•" + "JavaScript Project: " + this.Skills + "•" + "HTML Project:" + this.project +  "•" + "Nasa Project:"+ this.api; 
+//   }
+// };
 
 
-var i = 0;
-var txt = Iam.myPortfoilo();
-var speed = 60;
+// var i = 0;
+// var txt = Iam.myPortfoilo();
+// var speed = 60;
 
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("thediv").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
+// function typeWriter() {
+//   if (i < txt.length) {
+//     document.getElementById("thediv").innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
 
 
